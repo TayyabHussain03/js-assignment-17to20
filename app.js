8ef7389873c1cccdad4133bd2893992cfc65122b
TayyabHussain <tayyabfaisal106@gmail.com>
2021-05-19 19:55:46 +0500

    question1 added

---
+++ app.js
@@ -0,0 +1,13 @@
+//                           Assignment 17 to 20
+//                                Question 1
+// multidimesions empty 3
+// var data = [[], [], []];
+
+//                                Question 2
+
+// for (let i = 0; i < 5; i++) {
+
+    // for (let j = 0; j < 2; j++) {
+    //     console.log(i,j)
+    // }
+// }
